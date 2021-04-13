44600b2366ff4ac5a5fce6997d75767f235bfe5f
Luis <lquintanillab@gmail.com>
2021-04-13 13:19:39 -0500

    fix

--- cfdiPy/cfdiPy/settings.py
+++ cfdiPy/cfdiPy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-duo*(s^yuov-mx$sfj^rzup3=^j&4(9oa4j-jyg3&izckb-r^l'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
